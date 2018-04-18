868d59083dac79e47c25d57963f83da28586d75f
pengfei <pengfeiye1994@gmail.com>
2018-04-18 14:56:15 -0700

    login logic and page restriction complete

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -19,6 +20,15 @@ mysql = MySQL(app)
 
 Articles = Articles()
 
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please Login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
 def index():
@@ -29,11 +39,13 @@ def about():
     return render_template('about.html')
 
 @app.route('/articles')
+@is_logged_in
 def articles():
     return render_template('articles.html', articles = Articles)
 
 
 @app.route('/article/<string:id>')
+@is_logged_in
 def article(id):
     return render_template('article.html', id=id)
 
@@ -101,7 +113,7 @@ def login():
                 session['logged_in'] = True
                 session['username'] = username
 
-                flash('You are now logged in', 'sucess')
+                flash('You are now logged in', 'success')
 
                 return redirect(url_for('dashboard'))
             else:
@@ -116,10 +128,20 @@ def login():
 
     return render_template('login.html')
 
+#check to see if the user is logged in
+
 
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have logged out', 'success')
+
+    return redirect(url_for('index'))
 
 
 
